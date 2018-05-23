ad657ae77fd69ab76dadf36235512a06210a561f
pooi <ldayou@naver.com>
2018-05-23 12:39:47 +0900

    Add features   * Added check no hierarchical image accuracy module

---
+++ NoHierarchicalAccuracy.py
@@ -0,0 +1,315 @@
+import os, gc, shutil
+from PIL import Image
+
+import numpy as np
+import tensorflow as tf
+
+from threading import Thread
+from queue import Queue
+
+
+class Checker(Thread):
+
+    def __init__(self, num, queue, sess, output_operation, input_operation, labels, check_rates, base_accuracy, total_count, remove_list, summary):
+        Thread.__init__(self)
+        self.num = num
+        self.queue = queue
+        self.sess = sess
+        self.output_operation = output_operation
+        self.input_operation = input_operation
+        self.labels = labels
+        self.base_accuracy = base_accuracy
+        self.total_count = total_count
+        self.remove_list = remove_list
+        self.summary = summary
+        self.count = 0
+        self.check_rates = check_rates
+
+
+    def read_tensor_from_image_file(self, file_name, input_height=299, input_width=299,
+                                    input_mean=0, input_std=255):
+        input_name = "file_reader"
+        output_name = "normalized"
+        file_reader = tf.read_file(file_name, input_name)
+        if file_name.endswith(".png"):
+            image_reader = tf.image.decode_png(file_reader, channels=3,
+                                               name='png_reader')
+        elif file_name.endswith(".gif"):
+            image_reader = tf.squeeze(tf.image.decode_gif(file_reader,
+                                                          name='gif_reader'))
+        elif file_name.endswith(".bmp"):
+            image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
+        else:
+            image_reader = tf.image.decode_jpeg(file_reader, channels=3,
+                                                name='jpeg_reader')
+        del file_reader
+        file_reader = None
+        float_caster = tf.cast(image_reader, tf.float32)
+        del image_reader
+        image_reader = None
+        dims_expander = tf.expand_dims(float_caster, 0)
+        del float_caster
+        float_caster = None
+        resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+        del dims_expander
+        dims_expander = None
+        normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+        del resized
+        resized = None
+        sess = tf.Session()
+        # with tf.Session() as sess:
+        result = sess.run(normalized)
+        del normalized
+        normalized = None
+        sess.close()
+        del sess
+        sess = None
+
+        return result
+
+    def getMoveDir(self, fname):
+        fname = str(fname)
+        tempDirL = fname.split('/')[:-2]
+        tempDirR = fname.split('/')[-2:]
+
+        new_dir = ""
+        for i in range(len(tempDirL)):
+            new_dir += tempDirL[i]
+            if i + 1 < len(tempDirL):
+                new_dir += '/'
+            else:
+                new_dir += '_selected'
+
+            if not os.path.exists(new_dir):
+                os.mkdir(new_dir)
+                print("Create dir:", new_dir)
+
+        file_dir = ""
+        for i in range(len(tempDirR)):
+            file_dir += tempDirR[i]
+            if i + 1 < len(tempDirR):
+                file_dir += '/'
+
+        move_dir = os.path.join(new_dir, file_dir)
+        dirname = os.path.dirname(move_dir)
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+            print("Create dir:", dirname)
+
+        return move_dir
+
+    def check_image(self, key, fname):
+
+        try:
+            try:
+                # print(self.num, "1")
+                image = Image.open(fname)
+                image.load()
+                image.close()
+                format = image.format
+                if format == "JPEG" or format == "PNG":
+                    # print(self.num, "2")
+                    pass
+                else:
+                    print(self.num, "Not collect:", fname, " | Format error")
+                    # os.remove(fname)
+                    # self.remove_list.append(self.num)
+                    # self.summary[key]['notcollect'].append(self.num)
+                    # print(self.num, "3")
+                    return
+            except:
+                print(self.num, "Not collect:", fname, " | Format error")
+                # os.remove(fname)
+                # self.remove_list.append(self.num)
+                # self.summary[key]['notcollect'].append(self.num)
+                # print(self.num, "Del:", fname, " | Format error")
+                # os.remove(fname)
+                # self.remove_list.append(self.num)
+                # print(self.num, "4")
+                return
+
+            # print(self.num, "5")
+            t = self.read_tensor_from_image_file(fname)
+            # print(self.num, "6")
+
+            results = self.sess.run(self.output_operation.outputs[0],
+                               {self.input_operation.outputs[0]: t})
+            del t
+            t = None
+            # print(self.num, "7")
+            results = np.squeeze(results)
+            # print(self.num, "8")
+
+            top_k = results.argsort()[-5:][::-1]
+            # for i in top_k:
+            #     print(labels[i], results[i])
+
+            # print(self.num, "9")
+            if len(top_k) < 0:
+                print(self.num, "Not collect:", fname, " | Can't recognition")
+                # os.remove(fname)
+                # self.remove_list.append(self.num)
+                # self.summary[key]['notcollect'].append(self.num)
+                # print(self.num, "Del:", fname, " | Can't recognition")
+                # os.remove(fname)
+                # self.remove_list.append(self.num)
+                # print(self.num, "10")
+            else:
+                # isFinish = False
+                top_i = top_k[0]
+
+                top_label = self.labels[top_i]
+                top_result = results[top_i]
+
+                if key == top_label:
+                    for s in self.check_rates:
+                        collect = 'collect_' + str(s)
+                        notcollect = 'notcollect_' + str(s)
+                        if top_result >= s/100:
+                            self.summary[key][collect].append(self.num)
+                        else:
+                            self.summary[key][notcollect].append(self.num)
+                else:
+                    for s in self.check_rates:
+                        notcollect = 'notcollect_' + str(s)
+                        self.summary[key][notcollect].append(self.num)
+
+                # self.summary[key][top_label].append(self.num)
+
+                # for i in top_k:
+                #     label = self.labels[i]
+                #     result = results[i]
+                #
+                #     if label == key:
+                #
+                #         if result < 0.2:
+                #             # print(self.num, "Not collect:", fname, " | ", result)
+                #             self.summary[key]['notcollect_20'].append(self.num)
+                #             isFinish = True
+                #         else:
+                #             # print(self.num, "Collect:", fname, " | ", result)
+                #             self.summary[key]['collect_20'].append(self.num)
+                #             isFinish = True
+                #
+                #         if result < 0.3:
+                #             # print(self.num, "Not collect:", fname, " | ", result)
+                #             self.summary[key]['notcollect_30'].append(self.num)
+                #             isFinish = True
+                #         else:
+                #             # print(self.num, "Collect:", fname, " | ", result)
+                #             self.summary[key]['collect_30'].append(self.num)
+                #             isFinish = True
+                #
+                #         if result < 0.4:
+                #             # print(self.num, "Not collect:", fname, " | ", result)
+                #             self.summary[key]['notcollect_40'].append(self.num)
+                #             isFinish = True
+                #         else:
+                #             # print(self.num, "Collect:", fname, " | ", result)
+                #             self.summary[key]['collect_40'].append(self.num)
+                #             isFinish = True
+                #
+                #         if result < 0.5:
+                #             # print(self.num, "Not collect:", fname, " | ", result)
+                #             self.summary[key]['notcollect_50'].append(self.num)
+                #             isFinish = True
+                #         else:
+                #             # print(self.num, "Collect:", fname, " | ", result)
+                #             self.summary[key]['collect_50'].append(self.num)
+                #             isFinish = True
+                #
+                #         if result < 0.6:
+                #             # print(self.num, "Not collect:", fname, " | ", result)
+                #             self.summary[key]['notcollect_60'].append(self.num)
+                #             isFinish = True
+                #         else:
+                #             # print(self.num, "Collect:", fname, " | ", result)
+                #             self.summary[key]['collect_60'].append(self.num)
+                #             isFinish = True
+                #
+                #         if result < 0.7:
+                #             # print(self.num, "Not collect:", fname, " | ", result)
+                #             self.summary[key]['notcollect_70'].append(self.num)
+                #             isFinish = True
+                #         else:
+                #             # print(self.num, "Collect:", fname, " | ", result)
+                #             self.summary[key]['collect_70'].append(self.num)
+                #             isFinish = True
+                #
+                #         if result < 0.8:
+                #             # print(self.num, "Not collect:", fname, " | ", result)
+                #             self.summary[key]['notcollect_80'].append(self.num)
+                #             isFinish = True
+                #         else:
+                #             # print(self.num, "Collect:", fname, " | ", result)
+                #             self.summary[key]['collect_80'].append(self.num)
+                #             isFinish = True
+                #
+                #         if result < 0.9:
+                #             # print(self.num, "Not collect:", fname, " | ", result)
+                #             self.summary[key]['notcollect_90'].append(self.num)
+                #             isFinish = True
+                #         else:
+                #             # print(self.num, "Collect:", fname, " | ", result)
+                #             self.summary[key]['collect_90'].append(self.num)
+                #             isFinish = True
+                #
+                #         if result < 0.95:
+                #             # print(self.num, "Not collect:", fname, " | ", result)
+                #             self.summary[key]['notcollect_95'].append(self.num)
+                #             isFinish = True
+                #         else:
+                #             # print(self.num, "Collect:", fname, " | ", result)
+                #             self.summary[key]['collect_95'].append(self.num)
+                #             isFinish = True
+                #
+                #     if isFinish:
+                #         break
+                # print(self.num, "11")
+                del results
+                results = None
+                # if not isFinish:
+                #     print(self.num, "Not collect:", fname, " | Recognition fail -", self.labels[top_i])
+                    # os.remove(fname)
+                    # self.remove_list.append(self.num)
+                    # self.summary[key]['notcollect'].append(self.num)
+                    # print(self.num, "Del:", fname, " | Recognition fail -", self.labels[top_i])
+                    # os.remove(fname)
+                    # self.remove_list.append(self.num)
+                # print(self.num, "12")
+        except Exception as e:
+            print(e)
+            try:
+                print(self.num, "Not collect:", fname, " | Unknown error")
+                # os.remove(fname)
+                # self.remove_list.append(self.num)
+                # self.summary[key]['notcollect'].append(self.num)
+                # os.remove(fname)
+                # print(self.num, "Del:", fname, " | Unknown error")
+                # self.remove_list.append(self.num)
+            except:
+                pass
+
+
+    def run(self):
+        while True:
+            self.count += 1
+            if self.count % 13 == 0:
+                # self.sess.close()
+                # del self.sess
+                # self.sess = None
+                # self.sess = tf.Session(graph=self.graph)
+                # print(self.num, "Session create.", self.sess)
+                # gc.collect()
+                # print(self.num, "Execute Garbage Collector")
+                pass
+            # if self.count % 100 == 0:
+            #     gc.collect()
+            #     print(self.num, "Execute Garbage Collector")
+            data = self.queue.get()
+            if (self.total_count - self.queue.qsize()) % 5 == 0:
+                print(self.num, "Completed %d/%d" % (self.total_count - self.queue.qsize(), self.total_count))
+
+
+            self.check_image(data[0], data[1])
+            self.queue.task_done()
---
+++ nohierarchical_accuracy.py
@@ -0,0 +1,384 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os, time
+from PIL import Image
+import random
+
+import numpy as np
+import tensorflow as tf
+
+from queue import Queue
+from NoHierarchicalAccuracy import Checker
+
+
+def load_graph(model_file):
+    graph = tf.Graph()
+    graph_def = tf.GraphDef()
+
+    with open(model_file, "rb") as f:
+        graph_def.ParseFromString(f.read())
+    with graph.as_default():
+        tf.import_graph_def(graph_def)
+
+    return graph
+
+
+def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
+                                input_mean=0, input_std=255):
+    input_name = "file_reader"
+    output_name = "normalized"
+    file_reader = tf.read_file(file_name, input_name)
+    if file_name.endswith(".png"):
+        image_reader = tf.image.decode_png(file_reader, channels=3,
+                                           name='png_reader')
+    elif file_name.endswith(".gif"):
+        image_reader = tf.squeeze(tf.image.decode_gif(file_reader,
+                                                      name='gif_reader'))
+    elif file_name.endswith(".bmp"):
+        image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
+    else:
+        image_reader = tf.image.decode_jpeg(file_reader, channels=3,
+                                            name='jpeg_reader')
+    float_caster = tf.cast(image_reader, tf.float32)
+    dims_expander = tf.expand_dims(float_caster, 0)
+    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+    normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+    sess = tf.Session()
+    result = sess.run(normalized)
+    sess.close()
+
+    return result
+
+
+def load_labels(label_file):
+    label = []
+    proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+    for l in proto_as_ascii_lines:
+        label.append(l.rstrip())
+    return label
+
+
+def checkDir(dir_name, list):
+    # print("Dir: ", dir_name)
+    for dir in os.listdir(dir_name):
+        if dir.startswith("."):
+            continue
+
+        path = os.path.join(dir_name, dir)
+        if os.path.isdir(path):
+            checkDir(path, list)
+        else:
+            fname, ext = os.path.splitext(path)
+            ext = ext.lower()
+            if ext.endswith("jpeg") or ext.endswith("jpg") or ext.endswith("png") or ext.endswith("gif"):
+                list.append(path)
+
+
+def collectImage(dir_name, data, summary, check_rates):
+    # print("Dir: ", dir_name)
+    list = []
+    for dir in os.listdir(dir_name):
+        if dir.startswith("."):
+            continue
+
+        path = os.path.join(dir_name, dir)
+        if os.path.isdir(path):
+            collectImage(path, data, summary, check_rates)
+        else:
+            fname, ext = os.path.splitext(path)
+            ext = ext.lower()
+            if ext.endswith("jpeg") or ext.endswith("jpg") or ext.endswith("png") or ext.endswith("gif"):
+                list.append(path)
+
+    dir_name = dir_name.replace("\\", "/")
+    folder = dir_name.split("/").pop()
+    if len(list) > 0 and (not folder.startswith(".")):
+        data[folder] = list
+        temp = {}
+        for i in check_rates:
+            temp['collect_' + str(i)] = []
+            temp['notcollect_' + str(i)] = []
+
+        # for i in range(1, num_of_class+1):
+        #     class_key = "classification" + str(i)
+        #     temp[class_key] = []
+
+        summary[folder] = temp
+
+
+def printSummary(summary, check_rates):
+
+    hit_accuracy = {}
+    hit_total = []
+    print_format = "| %25s |"
+    print_list = []
+    print_list.append("Result:")
+    for i in check_rates:
+        print_list.append(str(i) + "%")
+        print_format += " %5s |"
+        hit_accuracy['hit' + str(i)] = []
+
+    # for i in range(1, num_of_class+1):
+    #     print_format += " %7s |"
+    #     print_list.append("class" + str(i))
+
+    print_format += " %7s |"
+    print_list.append("Hit")
+
+    num_of_divider = len(check_rates)*8 + 29 + 10
+
+    print("-"*num_of_divider)
+    print(print_format % tuple(print_list))
+    print("-" * num_of_divider)
+    # print("-" * 219)
+    # print("| %25s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s | %7s |" % ("Result:", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "95%", "class1", "class2", "class3", "class4", "class5", "class6", "class7", "class8", "class9", "class10", "2nd hit"))
+    # print("-" * 219)
+    for key in sorted(summary.keys()):
+        data = summary[key]
+
+        len_collect = len(data['collect_50'])
+        len_not_collect = len(data['notcollect_50'])
+        sum = len_collect + len_not_collect
+        if sum < 1:
+            continue
+
+        print_result_list = []
+        print_result_list.append(key)
+        print_result_format = "| %25s |"
+        for i in check_rates:
+            avg_key = "collect_" + str(i)
+            len_collect = len(data[avg_key])
+            len_not_collect = len(data["not" +avg_key])
+            sum = len_collect + len_not_collect
+            avg = len_collect / sum
+            hit_accuracy['hit' + str(i)].append(avg)
+            print_result_list.append(avg)
+            print_result_format += " %3.3f |"
+
+        # for i in range(1, num_of_class + 1):
+        #     print_result_format += " %7d |"
+        #     print_result_list.append(len(data['classification' + str(i)]))
+
+        hit_rate = len(data["collect_" + str(20)]) / (len(data["collect_" + str(20)]) + len(data["notcollect_" + str(20)]))
+        print_result_format += " %3.5f |"
+        print_result_list.append(hit_rate)
+        hit_total.append(hit_rate)
+
+
+        print(print_result_format % tuple(print_result_list))
+        # print("| %25s | %3.5f | %3.5f | %3.5f | %3.5f | %3.5f | %3.5f | %3.5f | %3.5f | %3.5f | %7d | %7d | %7d | %7d | %7d | %7d | %7d | %7d | %7d | %7d | %7d |" % (key, avg_20, avg_30, avg_40, avg_50, avg_60, avg_70, avg_80, avg_90, avg_95, len_class1, len_class2, len_class3, len_class4, len_class5, len_class6, len_class7, len_class8, len_class9, len_class10, len_second))
+    print("-" * num_of_divider)
+
+    print_format = "| %25s |"
+    print_list = []
+    print_list.append("Summary:")
+    for i in check_rates:
+        list = hit_accuracy['hit' + str(i)]
+        sum = 0
+        for l in list:
+            sum += l
+        print_format += " %3.3f |"
+        print_list.append(sum/len(list))
+
+    # for i in range(1, num_of_class+1):
+    #     print_format += " %7s |"
+    #     print_list.append("")
+
+    sum = 0
+    for l in hit_total:
+        sum += l
+    print_format += " %3.5f |"
+    print_list.append(sum / len(hit_total))
+
+    print(print_format % tuple(print_list))
+    print("-" * num_of_divider)
+
+if __name__ == "__main__":
+    file_name = "images/test.jpg"
+    model_file = "retrained_graph.pb"
+    label_file = "retrained_labels.txt"
+    input_height = 299
+    input_width = 299
+    input_mean = 0
+    input_std = 255
+    input_layer = "Mul"
+    output_layer = "final_result"
+
+    dirname = "./"
+    base_accuracy = 0.5
+    num_of_thread = 4
+    check_rates = [20, 30, 40, 50, 60, 70, 80, 90, 95, 98, 99]
+
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("--image", help="image to be processed")
+    parser.add_argument("--graph", help="graph/model to be executed")
+    parser.add_argument("--labels", help="name of file containing labels")
+    # parser.add_argument("--input_height", type=int, help="input height")
+    # parser.add_argument("--input_width", type=int, help="input width")
+    # parser.add_argument("--input_mean", type=int, help="input mean")
+    # parser.add_argument("--input_std", type=int, help="input std")
+    # parser.add_argument("--input_layer", help="name of input layer")
+    # parser.add_argument("--output_layer", help="name of output layer")
+    parser.add_argument("--dir", help="name of directory")
+    # parser.add_argument("--accuracy", help="set accuracy criteria")
+    parser.add_argument("--thread", help="number of thread")
+    args = parser.parse_args()
+
+    if args.dir:
+        dirname = args.dir
+
+    if args.thread:
+        try:
+            num_of_thread = int(args.thread)
+            if num_of_thread < 1:
+                num_of_thread = 4
+        except:
+            num_of_thread = 4
+
+
+    if args.graph:
+        model_file = args.graph
+    # if args.image:
+    #   file_name = args.image
+    if args.labels:
+        label_file = args.labels
+    # if args.input_height:
+    #   input_height = args.input_height
+    # if args.input_width:
+    #   input_width = args.input_width
+    # if args.input_mean:
+    #   input_mean = args.input_mean
+    # if args.input_std:
+    #   input_std = args.input_std
+    # if args.input_layer:
+    #   input_layer = args.input_layer
+    # if args.output_layer:
+    #   output_layer = args.output_layer
+
+    graph = load_graph(model_file)
+
+    input_name = "import/" + input_layer
+    output_name = "import/" + output_layer
+    input_operation = graph.get_operation_by_name(input_name)
+    output_operation = graph.get_operation_by_name(output_name)
+
+    # t = read_tensor_from_image_file(file_name,
+    #                                 input_height=input_height,
+    #                                 input_width=input_width,
+    #                                 input_mean=input_mean,
+    #                                 input_std=input_std)
+    labels = load_labels(label_file)
+
+    # with tf.Session(graph=graph) as sess:
+    try:
+        data = {}
+        summary = {}
+        collectImage(dirname, data, summary, check_rates)
+        for key in data.keys():
+            tempList = data[key]
+            rand_items = [tempList[random.randrange(len(tempList))] for item in range(200)]
+            data[key] = rand_items
+
+        total_image_count = 0
+        count = 0
+        remove_count = 0
+        remove_list = []
+        for key in data.keys():
+            total_image_count += len(data[key])
+
+        if len(data.keys()) > 1:
+            print("Found %d categories." % len(data.keys()))
+        else:
+            print("Found %d category." % len(data.keys()))
+        if total_image_count > 1:
+            print("Found %d images." % total_image_count)
+        else:
+            print("Found %d image." % total_image_count)
+
+        queue = Queue()
+        queueList = []
+        remove_list = []
+
+        # for i in range(num_of_thread):
+        #     checker = Checker(i, queue, graph, output_operation, input_operation, labels, base_accuracy, total_image_count, remove_list, summary)
+        #     checker.daemon = True
+        #     checker.start()
+
+
+        for key in sorted(data.keys()):
+            # print("Check", key)
+            list = data[key]
+            for fname in list:
+                item = []
+                item.append(key)
+                item.append(fname)
+                queueList.append(item)
+                # queue.put(item)
+
+        DIV = 100
+        length = len(queueList)
+        preQueueItem = ""
+        for step in range((length // DIV) + 1):
+            print("Start", (step+1), "/", ((length // DIV) + 1))
+            start_time = time.time()
+            queue = Queue()
+            threads = []
+            sess = tf.Session(graph=graph)
+
+            for i in range(num_of_thread):
+                checker = Checker(i, queue, sess, output_operation, input_operation, labels, check_rates, base_accuracy,
+                                  DIV, remove_list, summary)
+                checker.daemon = True
+                threads.append(checker)
+                checker.start()
+
+            for item in queueList[step * DIV : min((step + 1) * DIV, length)]:
+                if preQueueItem != item[0]:
+                    preQueueItem = item[0]
+                    print(preQueueItem)
+                queue.put(item)
+
+
+            queue.join()
+            del threads
+            sess.close()
+            sess.__del__()
+            print("Step", (step+1))
+            printSummary(summary, check_rates)
+            print("--- %s seconds ---" % (time.time() - start_time))
+            print()
+
+        printSummary(summary, check_rates)
+
+        print("Removed %d images" % len(remove_list))
+    except Exception as e:
+        print(e)
+    # finally:
+    #     sess.close()
+
+        # results = sess.run(output_operation.outputs[0],
+        #                   {input_operation.outputs[0]: t})
+        # results = np.squeeze(results)
+        #
+        # top_k = results.argsort()[-5:][::-1]
+        # labels = load_labels(label_file)
+        # for i in top_k:
+        #   print(labels[i], results[i])
